0432bec64cbf8ce6424f6abed926d42e7b6fee5d
nphamily <nganpham@Ngans-MacBook-Air.local>
2014-10-30 15:07:35 -0700

    collections_assessment

---
+++ collectionsassess.rb
@@ -0,0 +1,63 @@
+students = {
+  "John" => {
+    :first_name => "John",
+    :last_name => "Foley",
+    :email => "john@gschoo.it",
+    :class => "beginning snark"
+  },
+  "Sylwester" => {
+    :first_name => "Sylwester",
+    :last_name => "Kelsey",
+    :email => "sellie@gmail.com",
+    :class => "ruby immersive"
+  },
+
+  "Timonthy" => {
+    first_name: "Timothy",
+    last_name: "Rama",
+    email: "tim.rama@gmail.com",
+    class: "ruby immersive"
+  },
+  "Kane" => {
+    :first_name => "Kane",
+    :last_name => "Baccigalupi",
+    :email => "kane@gschool.it",
+    :class => "c for dummies"
+  },
+
+  "Nikita" => {
+    :first_name =>"Nikita",
+    :last_name => "Theodosius",
+    :email =>"nikita.theo@gmail.com",
+    :class => "ruby immersive"
+  },
+  "Roddy" =>{
+    :first_name => "Roddy",
+    :last_name => "Eldred",
+    :email => "roddy.el@gmail.com",
+    :class => "ruby immersive"
+  },
+
+  "Martha" => {
+    first_name: "Martha",
+    last_name: "Berner",
+    email: "martha@gschool.it",
+    class: "time traval for beginners"
+  },
+
+  "Kofi" => {
+    first_name: "Kofi",
+    last_name: "Thomas",
+    email: "k.thomas@hotmail.com",
+    class: "ruby immersive"
+    }
+}
+
+puts students["John"]
+puts students["Sylwester"]
+puts students["Timonthy"]
+puts students["Kane"]
+puts students["Nikita"]
+puts students["Roddy"]
+puts students["Martha"]
+puts students["Kofi"]
